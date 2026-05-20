bee00ae4a0e54ec46ed1d34147a39daafae519e7
ArturKremerTheiss <arturtheiss18@gmail.com>
2026-05-20 11:33:10 -0300

    banco de dados

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.meuapp;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,15 +10,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.sql.SQLDataException;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    SQLiteDatabase sqLiteDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-       getString(R.string.app_name);
 
+        sqLiteDatabase = openOrCreateDatabase("notas",MODE_PRIVATE, null);
+        sqLiteDatabase.execSQL("CREATE TABLE  IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, nota TEXT)");
+
+        //String titulo="João";
+        //sqLiteDatabase.execSQL("INSERT INTO notas VALUES(1,"+titulo+",'')");
+
+
+        ContentValues contentValues=new ContentValues();
+        contentValues.put("titulo","joão");
+        contentValues.put("nota", "777");
+        sqLiteDatabase.insert("notas", null,contentValues);
+
+
+        /// recuperar dados
+        sqLiteDatabase.rawQuery();
 
     }
 }
